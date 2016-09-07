8965527d56fb580ef7a374b93332b8fb42ed04fe
Ernest Fakudze <ernest.fakudze@scouts.org.uk>
2016-09-07 09:19:22 +0100

    Code tidy up.

--- Core/Common/Data/Interfaces/IObjectWithState.cs
+++ Core/Common/Data/Interfaces/IObjectWithState.cs
@@ -4,7 +4,7 @@
     /// An interface to make it easy to work with Entity Framework enties. Originally created by Julia Lerman
     /// See http://shop.oreilly.com/product/0636920022237.do?sortby=publicationDate
     /// All entities which are saved into the database by EF should implement this interface and also extend the BaseObjectWithState class. 
-    /// See <see cref="BaseObjectWithState"/> class. Also <see cref="EfDataRepositoryBase{T,TContext}"/> on how the interface and the base class are used.
+    /// See <see cref="BaseObjectWithState"/> class. Also <see cref="EfDataRepositoryBase"/> on how the interface and the base class are used.
     /// </summary>
     public interface IObjectWithState
     {
